42ad0bde25766fcaf29ca706ef21e47205f065ee
Matthew Potts <72045364+andromeda-voyager@users.noreply.github.com>
2021-05-27 09:31:07 -0600

    Create Config.java

---
+++ templates/Config.java
@@ -0,0 +1,7 @@
+package dev.matthewpotts.fulfiller;
+
+public class Config {
+    final static String SHOPIFY_API_URL = "https://.myshopify.com/admin/api/2021-04/";
+    final static String SHOPIFY_PASSWORD = "";
+    final static String SHOPIFY_API_KEY = "";
+}
